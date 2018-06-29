812d2b99f38c98966c9cec0df6e09a83a198950a
song5jing <1191861590@qq.com>
2018-06-29 09:44:50 +0800

    test

---
+++ test/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static  void print(int a,String name){
+        System.out.printf("%d,%s",a,name);
+    }
+
+    public static void main(String[] args) {
+        print(1,"abc");
+    }
+}
+
+
+
+
+
